cd03cf3a54023b7ab6cad69d1dfd5f12b1146751
uriacuesta <48221745+uriacuesta@users.noreply.github.com>
2020-06-16 18:12:30 -0400

    Create root.reducer.js

---
+++ root.reducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import someReducer from './someReducer.reducer';
+
+const rootReducer = combineReducers({
+  someReducer
+});
+
+export default rootReducer;
